96e7c5469625668db90f2d382e17ccbc0a6fbef4
任跃兵 <cnryb@live.cn>
2017-08-23 17:01:44 +0800

    init

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SevenZip
+namespace Test
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
